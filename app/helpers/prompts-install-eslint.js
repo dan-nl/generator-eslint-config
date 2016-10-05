91d043b3d339fb445eade0d4ef0e30116c748d4f
dan-nl <d_entous@yahoo.com>
2016-10-05 18:14:27 +0200

    re-worked prompting

--- app/helpers/prompts-install-eslint.js
+++ app/helpers/prompts-install-eslint.js
@@ -1,26 +1,24 @@
 'use strict';
 
 /**
- * @returns {Array}
+ * @type {*[]}
  */
-function getGeneratorPrompts() {
-  return [
-    {
-      default: true,
-      message: 'install eslint-config',
-      name: 'install-eslint',
-      type: 'confirm'
-    },
-    {
-      default: 'dan-nl/eslint-config-dan-nl',
-      message: 'eslint shared config on github',
-      name: 'eslint-shared-config',
-      type: 'input',
-      when: function ( answers ) {
-        return answers[ 'install-eslint' ];
-      }
+var prompts = [
+  {
+    default: true,
+    message: 'install eslint-config',
+    name: 'install-eslint',
+    type: 'confirm'
+  },
+  {
+    default: 'dan-nl/eslint-config-dan-nl',
+    message: 'eslint shared config on github',
+    name: 'eslint-shared-config',
+    type: 'input',
+    when: function ( answers ) {
+      return answers[ 'install-eslint' ];
     }
-  ];
-}
+  }
+];
 
-module.exports = getGeneratorPrompts;
+module.exports = prompts;
